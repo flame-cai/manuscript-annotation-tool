215d13a157005a2d69f6ac1f9b696225e983b703
Bharath Valaboju <bharath.valaboju@flame.edu.in>
2025-02-05 12:12:20 +0530

    feat: let users name their finetuned model

--- backend/annotator/finetune/finetune.py
+++ backend/annotator/finetune/finetune.py
@@ -14,35 +14,40 @@ from annotator.models import db, UserAnnotationLog
 
 BASE_PATH = "/mnt/cai-data/manuscript-annotation-tool/manuscripts"
 
-def get_config(file_path):
-    with open(file_path, 'r', encoding="utf-8") as stream:
+
+def get_config(file_path, manuscript_name, selected_model, model_name):
+    with open(file_path, "r", encoding="utf-8") as stream:
         opt = yaml.safe_load(stream)
     opt = AttrDict(opt)
-    if opt.lang_char == 'None':
-        characters = ''
-        for data in opt['select_data'].split('-'):
-            csv_path = os.path.join(opt['train_data'], data, 'labels.csv')
-            df = pd.read_csv(csv_path, sep='^([^,]+),', engine='python', usecols=['filename', 'words'], keep_default_na=False)
-            all_char = ''.join(df['words'])
-            characters += ''.join(set(all_char))
-        characters = sorted(set(characters))
-        opt.character= ''.join(characters)
-    else:
-        opt.character = opt.number + opt.symbol + opt.lang_char
-    os.makedirs(f'./saved_models/{opt.experiment_name}', exist_ok=True)
+    opt.character = opt.number + opt.symbol + opt.lang_char
+    opt.manuscript_name = manuscript_name
+    opt.saved_model = (
+        f"/mnt/cai-data/manuscript-annotation-tool/models/recognition/{selected_model}"
+    )
+    opt.model_name = model_name
+    os.makedirs(f"./saved_models/{opt.model_name}", exist_ok=True)
     return opt
 
-def finetune(annotations, app_context):
-    app_context.push()
 
-    opt = get_config(os.path.join("annotator", "finetune", "config_files", "config.yml"))
+def finetune(data):
+    manuscript_name = data[0]["manuscript_name"]
+    annotations = data[0]["annotations"]
+    selected_model = data[0]["selected_model"]
+    model_name = data[0].get("model_name", f"{manuscript_name}.pth")
+
+    opt = get_config(
+        os.path.join("annotator", "finetune", "config_files", "config.yml"),
+        manuscript_name,
+        selected_model,
+        model_name,
+    )
 
     TEMP_FOLDER = "temp"
     TRAIN_FOLDER = os.path.join(TEMP_FOLDER, "train")
     VAL_FOLDER = os.path.join(TEMP_FOLDER, "val")
     TRAIN_CSV_FILE = os.path.join(TRAIN_FOLDER, "labels.csv")
     VAL_CSV_FILE = os.path.join(VAL_FOLDER, "labels.csv")
-    
+
     # Ensure the temp folder exists
     os.makedirs(TRAIN_FOLDER, exist_ok=True)
     os.makedirs(VAL_FOLDER, exist_ok=True)
@@ -50,17 +55,16 @@ def finetune(annotations, app_context):
     # Initialize the CSV files with headers if they don't exist
     for csv_file in [TRAIN_CSV_FILE, VAL_CSV_FILE]:
         if not os.path.exists(csv_file):
-            with open(csv_file, mode='w', newline='') as csvfile:
+            with open(csv_file, mode="w", newline="") as csvfile:
                 csvwriter = csv.writer(csvfile)
                 csvwriter.writerow(["filename", "words"])
-    
-    manuscript_name = annotations[0]['manuscript_name']
-    annotations = annotations[0]['annotations']
 
     for page in annotations:
         for line in annotations[page]:
             ground_truth = annotations[page][line]["ground_truth"]
-            image_path = os.path.join(BASE_PATH, manuscript_name, "lines", page, line + ".jpg")
+            image_path = os.path.join(
+                BASE_PATH, manuscript_name, "lines", page, line + ".jpg"
+            )
             filename = os.path.basename(image_path)
 
             # Create log entry
@@ -90,9 +94,9 @@ def finetune(annotations, app_context):
                 print(f"Image not found: {image_path}")
 
             # Append to the appropriate CSV file
-            with open(target_csv, mode='a', newline='') as csvfile:
+            with open(target_csv, mode="a", newline="") as csvfile:
                 csvwriter = csv.writer(csvfile)
                 csvwriter.writerow([filename, ground_truth])
     db.session.commit()
-    
-    train(opt, amp=False, manuscript_name=manuscript_name)
+
+    train(opt, manuscript_name, amp=False)
--- frontend/src/views/AnnotationSection.vue
+++ frontend/src/views/AnnotationSection.vue
@@ -1,33 +1,40 @@
 <script setup>
-import { ref } from 'vue';
-import AnnotationPage from '@/components/AnnotationPage.vue';
-import { useAnnotationStore } from '@/stores/annotationStore';
+import { ref } from 'vue'
+import AnnotationPage from '@/components/AnnotationPage.vue'
+import { useAnnotationStore } from '@/stores/annotationStore'
 
 const props = defineProps(['recognitions'])
 const emit = defineEmits(['annotated'])
-const annotationStore = useAnnotationStore();
-const page = ref(Object.keys(props.recognitions).sort()[0]);
+const annotationStore = useAnnotationStore()
+const page = ref(Object.keys(props.recognitions).sort()[0])
 // const modelName = ref(annotationStore.modelName);
 
 function uploadGroundTruth() {
-  annotationStore.calculateLevenshteinDistances();
-  fetch(import.meta.env.VITE_BACKEND_URL + "/fine-tune", {
+  annotationStore.calculateLevenshteinDistances()
+  annotationStore.userAnnotations.forEach((elem) => {
+    elem["model_name"] = annotationStore.modelName;
+    console.log("added Model name", annotationStore.modelName)
+  })
+  fetch(import.meta.env.VITE_BACKEND_URL + '/fine-tune', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(annotationStore.userAnnotations)
-  }).then(()=>{
-    emit('annotated');
+    body: JSON.stringify(annotationStore.userAnnotations),
+  }).then(() => {
+    emit('annotated')
   })
 }
-
 </script>
 
 <template>
   <div class="mb-3">
     <label for="model-name" class="form-label">Model name</label>
-    <input class="form-control" placeholder="Name your model..." v-model="annotationStore.modelName">
+    <input
+      class="form-control"
+      placeholder="Name your model..."
+      v-model="annotationStore.modelName"
+    />
   </div>
   <div class="mb-3">
     <button class="btn btn-primary" @click="uploadGroundTruth">Fine-tune</button>
@@ -35,8 +42,19 @@ function uploadGroundTruth() {
   <div class="mb-3">
     <label for="page" class="form-label">Page</label>
     <select class="form-select" id="page" v-model="page" placeholder="Select a model">
-      <option v-for="(page_data, page_name) in props.recognitions" :key="page_name" :value="page_name">{{ page_name }}</option>
+      <option
+        v-for="(page_data, page_name) in props.recognitions"
+        :key="page_name"
+        :value="page_name"
+      >
+        {{ page_name }}
+      </option>
     </select>
   </div>
-  <AnnotationPage v-for="(page_data, page_name) in props.recognitions" :key="page_data" :data="page_data" v-show="page === page_name"/>
+  <AnnotationPage
+    v-for="(page_data, page_name) in props.recognitions"
+    :key="page_data"
+    :data="page_data"
+    v-show="page === page_name"
+  />
 </template>
