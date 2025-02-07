020f409ded1ace48982d683dd9d655d23436539f
Bharath Valaboju <bharath.valaboju@flame.edu.in>
2025-02-07 11:23:56 +0530

    added link to uploaded manuscripts

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -6,5 +6,6 @@ import { RouterLink } from 'vue-router';
   <header>
     <h1>Manuscript Annotation Tool</h1>
   </header>
-  <RouterLink :to="{name:'upload-manuscript'}" class="btn btn-primary">Annotate</RouterLink>
+  <RouterLink :to="{name:'upload-manuscript'}" class="btn btn-primary m-2">Annotate</RouterLink>
+  <RouterLink :to="{name: 'uploaded-manuscripts'}" class="btn btn-primary m-2">Uploaded Manuscripts</RouterLink>
 </template>
