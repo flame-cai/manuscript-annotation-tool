9334e3b04a989f5fa72f2e31f105b2418dcec183
Bharath Valaboju <bharath.valaboju@flame.edu.in>
2025-02-11 16:26:22 +0530

    added backend logic for manual segmentation

--- backend/annotator/routes.py
+++ backend/annotator/routes.py
@@ -2,6 +2,7 @@ import os
 import threading
 
 from flask import Blueprint, request, send_from_directory, current_app
+from PIL import Image
 
 from annotator.segmentation import segment_lines
 from annotator.recognition.recognition import recognise_characters
@@ -11,17 +12,23 @@ bp = Blueprint("main", __name__)
 
 BASE_PATH = "/mnt/cai-data/manuscript-annotation-tool/manuscripts"
 
+
 @bp.route("/")
 def hello():
     return "Sanskrit Manuscript Annotation Tool"
 
-@bp.route('/models')
+
+@bp.route("/models")
 def get_models():
     return os.listdir("/mnt/cai-data/manuscript-annotation-tool/models/recognition")
 
-@bp.route('/line-images/<string:manuscript_name>/<string:page>/<string:line>')
+
+@bp.route("/line-images/<string:manuscript_name>/<string:page>/<string:line>")
 def serve_line_image(manuscript_name, page, line):
-    return send_from_directory(os.path.join(BASE_PATH, manuscript_name, "lines", page), line + ".jpg")
+    return send_from_directory(
+        os.path.join(BASE_PATH, manuscript_name, "lines", page), line + ".jpg"
+    )
+
 
 @bp.route("/upload-manuscript", methods=["POST"])
 def annotate():
@@ -45,22 +52,28 @@ def annotate():
 
     return lines, 200
 
+
 def finetune_context(data, app_context):
     app_context.push()
     finetune(data)
 
-@bp.route('/fine-tune', methods=["POST"])
+
+@bp.route("/fine-tune", methods=["POST"])
 def do_finetune():
-    thread = threading.Thread(target=finetune_context, args=(request.json, current_app.app_context()))
+    thread = threading.Thread(
+        target=finetune_context, args=(request.json, current_app.app_context())
+    )
     thread.start()
     # finetune(request.json)
     return "Success", 200
 
-@bp.route('/uploaded-manuscripts', methods=["GET"])
+
+@bp.route("/uploaded-manuscripts", methods=["GET"])
 def get_manuscripts():
     return os.listdir("/mnt/cai-data/manuscript-annotation-tool/manuscripts")
 
-@bp.route('/recognise', methods=["POST"])
+
+@bp.route("/recognise", methods=["POST"])
 def recognise_manuscript():
     manuscript_name = request.json.get("manuscript_name")
     model = request.json.get("model")
@@ -71,3 +84,26 @@ def recognise_manuscript():
     lines = recognise_characters(folder_path, model, manuscript_name)
     print(lines)
     return lines, 200
+
+
+@bp.route("/segment/<string:manuscript_name>/<string:page>", methods=["GET"])
+def get_points(manuscript_name, page):
+    try:
+        IMAGE_FILEPATH = os.path.join(
+            BASE_PATH, manuscript_name, "leaves", f"{page}.jpg"
+        )
+        image = Image.open(IMAGE_FILEPATH)
+        width, height = image.size
+        response = {"dimensions": [width, height]}
+        POINTS_FILEPATH = os.path.join(
+            BASE_PATH, manuscript_name, "points-2D", f"{page}_points.txt"
+        )
+        if not os.path.exists(POINTS_FILEPATH):
+            return {"error": "Page not found"}, 404
+        with open(POINTS_FILEPATH, "r") as f:
+            points = [row.split() for row in f.readlines()]
+        response["points"] = points
+        return response, 200
+
+    except Exception as e:
+        return {"error": str(e)}, 500
