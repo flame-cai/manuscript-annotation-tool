b7d93c64f98087dcf69ce02c9edea2d4c8261ede
Bharath Valaboju <bharath.valaboju@flame.edu.in>
2025-02-17 16:13:39 +0530

    styled landing page

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -1,11 +1,38 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { useCssModule } from 'vue'
+import { RouterLink } from 'vue-router'
+const landingPage = useCssModule()
 </script>
 
 <template>
-  <header>
-    <h1>Manuscript Annotation Tool</h1>
-  </header>
-  <RouterLink :to="{name:'upload-manuscript'}" class="btn btn-primary m-2">Annotate</RouterLink>
-  <RouterLink :to="{name: 'uploaded-manuscripts'}" class="btn btn-primary m-2">Uploaded Manuscripts</RouterLink>
+  <div :class="landingPage['landing-container']">
+    <header>
+      <img src="../../public/flame-logo.svg" alt="Flame logo" :class="landingPage.logo" />
+      <h1>Manuscript Annotation Tool</h1>
+    </header>
+    <div :class="landingPage.links">
+      <RouterLink :to="{ name: 'upload-manuscript' }" class="btn btn-primary m-2"
+        >Annotate</RouterLink
+      >
+      <RouterLink :to="{ name: 'uploaded-manuscripts' }" class="btn btn-primary m-2"
+        >Uploaded Manuscripts</RouterLink
+      >
+    </div>
+  </div>
 </template>
+
+<style module>
+.landing-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.logo {
+  height: 35vh;
+  display: block;
+  margin: auto;
+}
+</style>
