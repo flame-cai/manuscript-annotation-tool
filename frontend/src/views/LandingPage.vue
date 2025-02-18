d03a404a487268f18c863a8815d5ee4adf249702
Bharath Valaboju <bharath.valaboju@flame.edu.in>
2025-02-18 10:28:57 +0530

    changed semantics of landing page

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -10,18 +10,21 @@ const landingPage = useCssModule()
       <img src="../../public/flame-logo.svg" alt="Flame logo" :class="landingPage.logo" />
       <h1>Manuscript Annotation Tool</h1>
     </header>
-    <div :class="landingPage.links">
-      <RouterLink :to="{ name: 'upload-manuscript' }" class="btn btn-primary m-2"
-        >Annotate</RouterLink
-      >
-      <RouterLink :to="{ name: 'uploaded-manuscripts' }" class="btn btn-primary m-2"
-        >Uploaded Manuscripts</RouterLink
-      >
-    </div>
+    <main :class="landingPage.main">
+      <div :class="landingPage.links">
+        <RouterLink :to="{ name: 'upload-manuscript' }" class="btn btn-primary m-2"
+          >Annotate</RouterLink
+        >
+        <RouterLink :to="{ name: 'uploaded-manuscripts' }" class="btn btn-primary m-2"
+          >Uploaded Manuscripts</RouterLink
+        >
+      </div>
+    </main>
   </div>
 </template>
 
 <style module>
+
 .landing-container {
   display: flex;
   flex-direction: column;
